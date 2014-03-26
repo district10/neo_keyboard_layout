667791ec2a42b2d2a5aa65f53eeb294e9bf49956
Gnat TANG <gnat_tang@yeah.net>
2014-03-26 09:43:13 +0800

    modified .sh

--- neo_us_dvpe/neo_us_dvpe.sh
+++ neo_us_dvpe/neo_us_dvpe.sh
@@ -1,9 +1,10 @@
 #/bin/sh bash 
 
+xcape -e "ISO_Group_Shift=space" \
+&& echo "xcape done" \
+|| echo "xcape troubled... Don't have xcape? Search it on Github"
+
 xmodmap ./neo_us_dvpe.xmodmap \
 && echo "neo-extended dvpe done" \
 || echo "neo-extended dvpe troubled"
 
-xcape -e "ISO_Group_Shift=space" \
-&& echo "xcape done" \
-|| echo "xcape troubled... Don't have xcape? Search it on Github"
--- neo_us_qwerty/neo_us_qwerty.sh
+++ neo_us_qwerty/neo_us_qwerty.sh
@@ -1,9 +1,10 @@
 #/bin/sh bash 
 
+xcape -e "ISO_Group_Shift=space" \
+&& echo "xcape done" \
+|| echo "xcape troubled... Don't have xcape? Search it on Github"
+
 xmodmap ./neo_us_qwerty.xmodmap \
 && echo "neo-extended qwerty done" \
 || echo "neo-extended qwerty troubled"
 
-xcape -e "ISO_Group_Shift=space" \
-&& echo "xcape done" \
-|| echo "xcape troubled... Don't have xcape? Search it on Github"
