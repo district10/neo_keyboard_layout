925d5503761a84f453ab2716ded60a833d6a7d13
Gnat TANG <gnat_tang@yeah.net>
2014-04-02 12:07:23 +0800

    change it back

--- neo_us_dvp/neo_us_dvp.sh
+++ neo_us_dvp/neo_us_dvp.sh
@@ -1,10 +1,9 @@
 #/bin/sh bash 
 
-xcape -e "ISO_Group_Shift=space" \
-&& echo "xcape done" \
-|| echo "xcape troubled... Don't have xcape? Search it on Github"
-
 xmodmap ./neo_us_dvp.xmodmap \
 && echo "neo-extended dvp done" \
 || echo "neo-extended dvp troubled"
 
+xcape -e "ISO_Group_Shift=space" \
+&& echo "xcape done" \
+|| echo "xcape troubled... Don't have xcape? Search it on Github"
