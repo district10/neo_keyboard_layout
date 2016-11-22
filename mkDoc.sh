76921288a6061f96863bf7a42f060f8577aeab27
TANG ZhiXiong <dvorak4tzx@gmail.com>
2016-11-22 09:42:43 +0800

    added neo plain for windows

--- mkDoc.sh
+++
@@ -1,8 +0,0 @@
-pandoc -S -s --ascii \
--c http://tangzx.qiniudn.com/main.css \
---highlight-style pygments \
--f markdown+table_captions README.md -o index.html
-
-# -A ../_parts/footer.html \
-# --toc \
-# --mathjax \
